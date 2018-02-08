69a27b5caf35570f063de0074f73e9ec09f9f9cd
bahar.gezici <bahar.gezici@veon.com>
2018-02-09 00:23:14 +0100

    tests are added

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "LearningSpark"
+name := "SparkWordCount"
 
 version := "1.0"
 
@@ -13,16 +13,7 @@ scalacOptions ++= Seq("-unchecked", "-deprecation")
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0"
-
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
-
-libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.2.0"
-
-libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.2.0"
-
-// needed to make the hiveql examples run at least on Linux
-javaOptions in run += "-XX:MaxPermSize=128M"
 
 scalacOptions += "-target:jvm-1.8"
 
---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,60 @@
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{count, desc}
+
+object WordCount {
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkSession: SparkSession = setUp
+
+    val files = "t1.txt" :: "t2.txt" :: Nil
+    for( i <- 0 to files.length-1){
+      createDataSet(sparkSession, files(i), "resultSet".concat(i.toString))
+    }
+
+    sparkSession.sql("SELECT NVL(a.value, b.value) as word, (NVL(a.count,0) + NVL(b.count,0)) as total_occurrences, " +
+                    "NVL(a.count,0) as occurrences_file1, NVL(b.count,0) as occurrences_file2 " +
+                    "FROM resultSet0 a " +
+                    "FULL OUTER JOIN resultSet1 b " +
+                    "ON a.value = b.value " +
+                    "ORDER BY 2 DESC")
+                .show()
+
+  }
+
+  /** Creates a dataset with a given filename
+    *
+    *  @param sparkSession
+    *  @param fileName
+    *  @param datasetName
+    */
+  private def createDataSet(sparkSession: SparkSession, fileName: String, datasetName: String) = {
+    import sparkSession.implicits._
+    val ds = sparkSession.read.text("./src/main/resources/data/" + fileName).as[String]
+    val result = ds
+      .flatMap(_.trim.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+")) // Split on whitespace
+      .filter(_ != "") // Filter empty words
+      .toDF() // to DataFrame
+      .groupBy("value") // Group by word
+      .agg(count("*") as "count") // Number of occurences of each word
+      .orderBy(desc("count"))
+    result.createOrReplaceTempView(datasetName)
+  }
+
+  /** Creates Spark Session and sets conf
+    * Whereas in Spark 2.0 the same effects can be achieved through SparkSession,
+    * without expliciting creating SparkConf, SparkContext or SQLContext,
+    * as they’re encapsulated within the SparkSession
+    *
+    */
+  private def setUp = {
+    val sparkSession = SparkSession.builder.
+      master("local")
+      .appName("example")
+      .getOrCreate()
+
+    sparkSession.conf.set("spark.sql.shuffle.partitions", 2)
+    sparkSession.conf.set("spark.executor.memory", "2g")
+    sparkSession
+  }
+}
